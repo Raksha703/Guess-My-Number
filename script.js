b045f8afc89fab8b971644ea053008c84c0a5e9d
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-07-17 02:33:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent="🎉 Correct number!!";
+
+document.querySelector(".guess").value=36;
+
+document.querySelector(".score").textContent=20;
+*/
+
 let magicNumber = Math.trunc(Math.random()*20) + 1;
 let sc = 20;
 let highsc = document.querySelector(".highscore").textContent;
@@ -12,33 +21,30 @@ document.querySelector(".check").addEventListener(
         if(!guessedNumber){
             document.querySelector(".message").textContent = "🚨 No Number";
         }
+
+        else if(chance==0){
+            document.querySelector(".message").textContent = "🥴 You Lost the Game";
+            document.getElementById(".check").disabled = true;
+            sc=0;
+            chance=0;
+        }
+
         else if(guessedNumber>magicNumber){
-            if(chance==1){
-                document.querySelector(".message").textContent = "🥴 You Lost the Game";
-                sc=0;
-                chance=0;
-            }
-            else{
-                document.querySelector(".message").textContent = "📈 Too High";
-                sc--;
-                chance--;
-            }
+            document.querySelector(".message").textContent = "📈 Too High";
+            sc--;
+            chance--;
         }
+
         else if(guessedNumber<magicNumber){
-            if(chance==1){
-                document.querySelector(".message").textContent = "🥴 You Lost the Game";
-                sc=0;
-                chance=0;
-            }
-            else{
-                document.querySelector(".message").textContent = "📉 Too Low";
-                sc--;
-                chance--;
-            }
+            document.querySelector(".message").textContent = "📉 Too Low";
+            sc--;
+            chance--;
         }
+
         else{
             document.querySelector(".message").textContent = "🎉 Correct number!!";
             document.querySelector(".number").textContent = magicNumber;
+            document.getElementById(".check").disabled = true;
             sc++;
 
             if(sc>highsc){
@@ -58,7 +64,7 @@ document.querySelector(".check").addEventListener(
 
 document.querySelector(".again").addEventListener(
     "click", function(){
-        let highsc = document.querySelector(".highscore").textContent;
+        highsc = document.querySelector(".highscore").textContent;
         magicNumber = Math.trunc(Math.random()*20) + 1;
         sc=20;
         chance=5;
@@ -72,5 +78,6 @@ document.querySelector(".again").addEventListener(
         document.querySelector(".guess").value = "";
         document.querySelector(".highscore").textContent = highsc;
         document.querySelector(".chance").textContent = chance;
+        document.getElementById(".check").disabled = false;
     }
 );
